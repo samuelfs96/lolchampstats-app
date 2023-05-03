45df56772d1862147bcbd4f21107eddd67ca6182
Samuel <samuelfs96@gmail.com>
2023-05-03 13:57:56 -0400

    favicon changes

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -13,7 +13,7 @@ export default function Layout({
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={`${styles.main} ${inter.className}`}>
+      <main className={`${styles.main}`}>
         {children}
       </main>
     </>
