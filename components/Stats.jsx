7e0a732b7e076002b45a1d8934c455cb05a83a86
Samuel <samuelfs96@gmail.com>
2023-05-08 21:36:07 -0400

    stats for champions

---
+++ components/Stats.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+import {
+    Chart as ChartJS,
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import { Radar } from 'react-chartjs-2';
+
+ChartJS.register(
+    RadialLinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+  );
+
+export default function Stats({labels, data}) {
+    console.log({labels, data})
+    return <Radar
+    data={{
+        labels,
+        datasets: [{
+        label: 'Value',
+        data,
+        fill: true,
+        backgroundColor: [
+            'rgb(187 49 98 / 50%)',
+        ],
+        borderColor: [
+            '#bb3162',
+        ],
+        }]
+    }} options={{
+        scales: {
+            r: {
+                grid: {
+                    color: 'rgba(54, 162, 235, 0.25)'
+                },
+                angleLines: {
+                    color: 'rgba(54, 162, 235, 0.25)'
+                },
+                ticks: {
+                    display: false,
+                },
+                min: 10,
+                max: 100
+            }
+          },
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                callbacks: {
+                    label: function(value) {
+                        return value.formattedValue + '%';
+                    }
+                }
+            }
+        }
+    }} />;
+}
--- pages/champion/[id].jsx
+++ pages/champion/[id].jsx
@@ -6,12 +6,13 @@ import { styled } from '@mui/material/styles';
 import React from 'react'
 import localFont from 'next/font/local';
 import Carousel from '@/components/Carousel';
+import Stats from '@/components/Stats';
 
 const myFont = localFont({ src: '../../public/fonts/Azonix.otf' });
 const API_VERSION = process.env.NEXT_PUBLIC_API_VERSION;
 
 const HtmlTooltip = styled(({ className, ...props }) => (
-    <Tooltip {...props} classes={{ popper: className }} enterTouchDelay={0} />
+    <Tooltip {...props} classes={{ popper: className }} enterTouchDelay={0} placement="bottom" />
   ))(({ theme }) => ({
     [`& .${tooltipClasses.tooltip}`]: {
       backgroundColor: '#000',
@@ -27,12 +28,9 @@ const HtmlTooltip = styled(({ className, ...props }) => (
 const ToltipInfo = ({name, description}) => {
     return (
         <>
-        <div dangerouslySetInnerHTML={{__html: name}} style={{marginBottom: '.75rem', fontSize: '.8rem'}} className={myFont.className}></div>
-        <div dangerouslySetInnerHTML={{__html: description}} style={{marginBottom: '0', fontSize: '.5rem', lineHeight: '.75rem'}} className={myFont.className}></div>
-            {/* <h2 style={{marginTop: '0', fontSize: '.8rem'}} className={myFont.className}>{name}</h2>
-            <p style={{marginBottom: '0', fontSize: '.5rem', lineHeight: '.75rem'}} className={myFont.className}>{description}</p>
-         */}
-         </>
+            <div dangerouslySetInnerHTML={{__html: name}} style={{marginBottom: '.75rem', fontSize: '.8rem'}} className={myFont.className}></div>
+            <div dangerouslySetInnerHTML={{__html: description}} style={{marginBottom: '0', fontSize: '.5rem', lineHeight: '.75rem'}} className={myFont.className}></div>
+        </>
     )
 }
 
@@ -44,9 +42,16 @@ const skins = (skins, champ) => {
     }));
 }
 
+const getData = (stats) => {
+    return {
+        labels: Object.keys(stats).map(stat => stat.toUpperCase()), 
+        data: Object.values(stats).map(stat => stat)
+    };
+}
+
 export default function index({champion}) {
   return (
-    <Layout title="Lol Champion Stats">
+    <Layout title={`${champion?.name} - Lol Champion Stats`}>
         <Box sx={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
             <ChampImage champ={champion?.id} type='splash' style={{width: '100%', filter: 'opacity(0.5)'}}/>
             <Box sx={{color: 'white', textAlign: 'center', position: 'absolute', fontSize: {xs: '1rem', sm: '2rem'}, '& *': {margin: '14px'}}}>
@@ -67,7 +72,7 @@ export default function index({champion}) {
                 borderColor: 'primary.pink'
             }}>
                 <Box>
-                    <h2 style={{marginTop: '0'}}>Rol</h2>
+                    <h2 style={{marginTop: '0'}}>Role</h2>
                     <Stack direction="row" spacing={1}>
                         {champion?.tags.map((tag, index) => <Chip key={tag} label={tag} className={myFont.className} 
                             sx={{
@@ -89,42 +94,58 @@ export default function index({champion}) {
             <Box sx={{
                 display: 'flex', 
                 justifyContent: 'center',
-                flexDirection: 'column', 
                 border: '1px solid', 
                 padding: '1.75rem', 
                 marginTop: '4rem',
                 fontSize: '1rem',
                 borderColor: 'primary.blue',
-                textAlign: 'center'
+                textAlign: 'center',
+                gap: '2rem'
             }}>
-                <h2 style={{margin: 0}}>Spells</h2>
-                <Box sx={{display: 'flex', justifyContent: 'center', flexFlow: 'wrap', marginTop: '1.5rem', gap: '1rem'}}>
-                    <HtmlTooltip key={champion?.passive?.name} title={<ToltipInfo name={champion?.passive?.name} description={champion?.passive?.description} />} placement="top">
-                        <Box sx={{border: '1px solid white', '&:hover': {
-                          opacity: '0.8',
-                          transform: 'scale(1.05)',
-                          transition: '.3s all ease'
-                        }}}>
-                            <img alt={`Image of ${champion?.passive?.name}`}
-                                src={`https://ddragon.leagueoflegends.com/cdn/${API_VERSION}/img/passive/${champion?.passive?.image.full}`}/>
-                            <p style={{margin: '.25rem'}}>passive</p>
-                        </Box>
-                    </HtmlTooltip>
-                    {
-                        champion?.spells.map(spell => 
-                            <HtmlTooltip key={spell.id} title={<ToltipInfo name={spell?.name} description={spell?.description} />} placement="top">
-                                <Box sx={{border: '1px solid white', '&:hover': {
-                                    opacity: '0.8',
-                                    transform: 'scale(1.05)',
-                                    transition: '.3s all ease'
-                                }}}>
-                                    <img  alt={`Image of ${spell.id}`} 
-                                        src={`https://ddragon.leagueoflegends.com/cdn/${API_VERSION}/img/spell/${spell.image.full}`}/>
-                                    <p style={{margin: '.25rem'}}>{spell.id.split('').pop()}</p>
-                                </Box>
-                            </HtmlTooltip>
-                        )
-                    }
+                <Box sx={{width: '50%'}}>
+                    <h2 style={{margin: 0}}>Spells</h2>
+                    <Box sx={{display: 'flex', justifyContent: 'center', flexFlow: 'wrap', marginTop: '1.5rem', gap: '1rem'}}>
+                        <HtmlTooltip key={champion?.passive?.name} title={<ToltipInfo name={champion?.passive?.name} description={champion?.passive?.description} />}>
+                            <Box sx={{border: '1px solid white', '&:hover': {
+                            opacity: '0.8',
+                            transform: 'scale(1.05)',
+                            transition: '.3s all ease'
+                            }}}>
+                                <img alt={`Image of ${champion?.passive?.name}`}
+                                    src={`https://ddragon.leagueoflegends.com/cdn/${API_VERSION}/img/passive/${champion?.passive?.image.full}`}/>
+                                <p style={{margin: '.25rem'}}>passive</p>
+                            </Box>
+                        </HtmlTooltip>
+                        {
+                            champion?.spells.map(spell => 
+                                <HtmlTooltip key={spell.id} title={<ToltipInfo name={spell?.name} description={spell?.description} />} placement="top">
+                                    <Box sx={{border: '1px solid white', '&:hover': {
+                                        opacity: '0.8',
+                                        transform: 'scale(1.05)',
+                                        transition: '.3s all ease'
+                                    }}}>
+                                        <img  alt={`Image of ${spell.id}`} 
+                                            src={`https://ddragon.leagueoflegends.com/cdn/${API_VERSION}/img/spell/${spell.image.full}`}/>
+                                        <p style={{margin: '.25rem'}}>{spell.id.split('').pop()}</p>
+                                    </Box>
+                                </HtmlTooltip>
+                            )
+                        }
+                    </Box>
+                </Box>
+                <Divider orientation="vertical" variant="middle" flexItem sx={{borderColor: 'primary.blue', display: {xs: 'none', md: 'block'}}}/>
+                <Box sx={{display: 'flex', justifyContent: 'center'}}>
+                    <h2 style={{margin: 0}}>Stats</h2>
+                    <Box sx={{width: {xs: '80vw', md: '400px'}}}><Stats {
+                        ...getData({
+                            ...Object.entries(champion?.info).reduce((newObject, object) => {
+                                const [key, value] = object;
+                                newObject[key] = value * 10;
+                                return newObject
+                            }, {}), 
+                            speed: parseInt(champion?.stats.movespeed * 100 / 425)
+                        })
+                    }/></Box>
                 </Box>
             </Box>
             <Box sx={{
